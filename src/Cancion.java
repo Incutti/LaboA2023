40d827ad47e91fd2cea7157881aac1b2a06afe54
Incutti <mateincu@gmail.com>
2023-03-20 11:59:19 -0300

    Ejercicio cancion

---
+++ src/Cancion.java
@@ -0,0 +1,48 @@
+public class Cancion {
+    private String titulo;
+    private Persona autor;
+    private float duracion;
+    private String genero;
+
+    public Cancion(String titulo, Persona autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public Cancion() {
+        titulo = "titulo";
+        autor = new Persona();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Persona getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Persona autor) {
+        this.autor = autor;
+    }
+
+    public float getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(float duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+}
