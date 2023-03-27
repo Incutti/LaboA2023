ce109890a9ca3523d25b7ac4b167acf486362c89
Incutti <mateincu@gmail.com>
2023-03-27 11:58:02 -0300

    Ejercicio CD (4) UNIDAD2

--- src/CD.java
+++ src/CD.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+
 public class CD {
+    private ArrayList<Cancion> canciones;
+
+    public CD() {
+        canciones = new ArrayList<Cancion>();
+    }
+
+    public ArrayList<Cancion> getCanciones() {
+        return canciones;
+    }
+
+    public void setCanciones(ArrayList<Cancion> canciones) {
+        this.canciones = canciones;
+    }
+
+    int numeroCanciones() {
+        int contador = 0;
+        for (Cancion cancion : canciones) {
+            contador++;
+        }
+        return contador;
+    }
+
+    void verCancion(int posicion) {
+        System.out.println(canciones.get(posicion).getTitulo());
+    }
+
+    void grabaCancion(int posicion, Cancion nuevaCancion) {
+        canciones.set(posicion, nuevaCancion);
+    }
+
+    void agrega(Cancion cancion) {
+        canciones.add(cancion);
+    }
+
+    void elimina(int posicion) {
+        canciones.remove(posicion);
+    }
 }
