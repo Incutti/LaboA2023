60f1cf2e049e5c9b0e0b78553ba7b12bc277765f
Incutti <mateincu@gmail.com>
2023-03-27 10:01:09 -0300

    Ejercicios 1, 2 y 3 de UNIDAD2 (en el main)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // UNIDAD 1
+        /*
         Libro libro1=new Libro();
         Libro libro=new Libro("cualquier cosa",new Persona(),122341,124124,"string",new Fecha(12,12,2012));
         libro.mostrarInfo();
@@ -22,6 +27,36 @@ public class Main {
         System.out.println(cancion.getAutor().getNombre());
         System.out.println(cancion.getDuracion());
         System.out.println(cancion.getGenero());
+        */
+        // UNIDAD 2
 
+        // EJERCICIO 1
+        ArrayList<Integer> numeros = new ArrayList<Integer>();
+        numeros.add(2);
+        numeros.add(4);
+        int suma = 0;
+        for (Integer numero : numeros) {
+            suma = suma + numero;
+        }
+        System.out.println(suma);
+
+        //EJERCICIO 2
+        char letraEspecifica = 'a';
+        ArrayList<String> palabras = new ArrayList<String>();
+        palabras.add("ariana");
+        palabras.add("mariana");
+        for (String palabra : palabras) {
+            if (palabra.charAt(0) == letraEspecifica) {
+                System.out.println(palabra);
+            }
+        }
+
+        //EJERCICIO 3
+        ArrayList<Persona>personas=new ArrayList<Persona>();
+        for(Persona persona:personas){
+            if(persona.getEdad()>30){
+                persona.impresionDeHumanos();
+            }
+        }
     }
 }
