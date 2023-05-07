ec575f5e0bd0d9193e9b9bf162656bb50fc57185
Incutti <mateincu@gmail.com>
2023-05-07 16:10:11 -0300

    correccion del getter de variable boolean

--- src/EjercicioRepaso1/EquipoSonido.java
+++ src/EjercicioRepaso1/EquipoSonido.java
@@ -3,7 +3,7 @@ package EjercicioRepaso1;
 public class EquipoSonido extends SeccionMultimedia {
     private boolean moduloBluetooth;
 
-    public boolean getModuloBluetooth(){
+    public boolean isModuloBluetooth(){
         return moduloBluetooth;
     }
     public void setModuloBluetooth(boolean moduloBluetooth){
