39ebc47e327cfcde883f6d76a16cfeac9da82b3e
Incutti <mateincu@gmail.com>
2023-08-23 08:28:03 -0300

    se agrego constructor por defecto

--- src/ABMMascotas/Duenio.java
+++ src/ABMMascotas/Duenio.java
@@ -3,6 +3,9 @@ package ABMMascotas;
 import Humanos.Persona;
 
 public class Duenio extends Persona {
+    public Duenio(){
+        super();
+    }
     public Duenio(String nombre,String apellido){
         super(nombre,apellido);
     };
