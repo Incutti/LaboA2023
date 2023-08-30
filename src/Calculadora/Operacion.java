e1419d94266b5e186428d9c6ef9bae6e98b5b634
Incutti <mateincu@gmail.com>
2023-08-30 08:34:26 -0300

    ejercicio 3 de calculadora

---
+++ src/Calculadora/Operacion.java
@@ -0,0 +1,17 @@
+package Calculadora;
+
+public class Operacion {
+    public static double sumar(double num1, double num2){
+        return num1+num2;
+    }
+    public static double restar(double num1, double num2){
+        return num1-num2;
+    }
+
+    public static void main(String args[]){
+        System.out.print("La suma de 2+4 es ");
+        System.out.println(Operacion.sumar(2, 4));
+        System.out.print("La resta de 6-2 es ");
+        System.out.println(Operacion.restar(6, 2));
+    }
+}
